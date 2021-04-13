2e6e657755d1076d931b4dfa0eaed81c638cc87f
Takayuki Sei <takayuki.sei@gree.net>
2021-04-13 12:39:45 +0900

    add xcodeproj

---
+++ StudyCreateML/StudyCreateML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyCreateML
+//
+//  Created by Takayuki Sei on 2021/04/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
